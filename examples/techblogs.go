3d5a4bfd8e882e39560ff7352e29d2594de4a8ce
Jacek Nosal <jacek.nosal@outlook.com>
2017-07-03 20:53:59 +0200

    Added TechCrunch Scraper

--- examples/techblogs.go
+++ examples/techblogs.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	//"github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 	"gotana"
 	"strings"
 )
@@ -42,6 +43,23 @@ func ParseSpotify(proxy gotana.ScrapedItem, items chan<- gotana.SaveableItem) {
 	}
 }
 
+func ParseTechChrunch(proxy gotana.ScrapedItem, items chan<- gotana.SaveableItem) {
+	defer gotana.SilentRecover("ParseTechChrunch")
+
+	if gotana.ContainsOneOf(proxy.Url, []string{"/page/"}) {
+		gotana.Logger().Debug(proxy)
+		document, err := proxy.HTMLDocument()
+		if err != nil {
+			gotana.Logger().Error(err.Error())
+			return
+		}
+
+		document.Find(".post-title a").Each(func(i int, s *goquery.Selection) {
+			gotana.Logger().Noticef("%s", s.Text())
+		})
+	}
+}
+
 func main() {
 	config := gotana.NewSpiderConfig("techblogs.yml")
 	engine := gotana.NewEngine()
@@ -50,6 +68,7 @@ func main() {
 
 	engine.GetScraper("pornhub").SetHandler(ParsePornHub)
 	engine.GetScraper("spotify").SetHandler(ParseSpotify)
+	engine.GetScraper("techcrunch").SetHandler(ParseTechChrunch)
 
 	engine.Start()
 }
